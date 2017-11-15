7837257f439afa7aba5c8f4b1c197297e23d105b
ivan.santiago6 <ivan.santiago6@coco.ece.uprm.edu>
2017-11-15 12:36:32 -0400

    Frame centered to screen (local commit)

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -1,11 +1,15 @@
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
 import javax.swing.JFrame;
 
 public class Lab3Exercises {
 	public static void main(String[] args) {
-		JFrame myFrame = new JFrame("Hello, world!");
-		//myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		myFrame.setLocation(200, 200);
-		myFrame.setSize(400, 100);
+		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		JFrame myFrame = new JFrame("Ivan Santiago De Jesus");
+		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		myFrame.setSize(200, 200);
+		myFrame.setLocation((int)((screenSize.getWidth()-myFrame.getWidth())/2), (int)((screenSize.getHeight()-myFrame.getHeight())/2));
 		myFrame.setVisible(true);
 	}
 }
