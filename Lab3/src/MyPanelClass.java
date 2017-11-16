91dfe2e9d6f79bc4d74cf038f2e27578cc344b62
Ivan <ivan.santiago6@upr.edu>
2017-11-15 23:21:07 -0400

    Added Puerto Rico flag.

--- Lab3/src/MyPanelClass.java
+++ Lab3/src/MyPanelClass.java
@@ -1,6 +1,8 @@
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Insets;
+import java.awt.Polygon;
+
 import javax.swing.JPanel;
  
 public class MyPanelClass extends JPanel {
@@ -17,10 +19,44 @@ public class MyPanelClass extends JPanel {
                         int height = y2 - y1;
  
                         //Paint the background
-                        g.setColor(Color.BLACK);
+                        g.setColor(Color.WHITE);
                         g.fillRect(x1, y1, width + 1, height + 1);
                         
-                      //Draw a border
+                        //Red stripes
+                        g.setColor(Color.RED);
+                        g.fillRect(x1, y1, width, height/5);
+
+                        g.setColor(Color.RED);
+                        g.fillRect(x1, height/3+height/14, width, height/5);
+                        
+                        g.setColor(Color.RED);
+                        g.fillRect(x1, height-(height/5), width, height/5);
+                        
+                        //Blue triangle
+                        Polygon p = new Polygon();
+                        p.addPoint(x1, y1);
+                        p.addPoint(width/2, height/2);
+                        p.addPoint(x1, height);
+                        g.setColor(new Color(135,206,250));
+                        g.fillPolygon(p);
+                        
+                        //White star
+                        Polygon p2 = new Polygon();
+                        p2.addPoint(x1 + 25, y1 + 73);
+                        p2.addPoint(x1 + 41, y1 + 73);
+                        p2.addPoint(x1 + 47, y1 + 58);
+                        p2.addPoint(x1 + 53, y1 + 73);
+                        p2.addPoint(x1 + 69, y1 + 73);
+                        p2.addPoint(x1 + 56, y1 + 83);
+                        p2.addPoint(x1 + 61, y1 + 98);
+                        p2.addPoint(x1 + 47, y1 + 88);
+                        p2.addPoint(x1 + 34, y1 + 98);
+                        p2.addPoint(x1 + 38, y1 + 83);
+                        p2.translate(width/10, height/3);
+                        g.setColor(Color.WHITE);
+                        g.fillPolygon(p2);
+                      
+                      /*//Draw a border
                         g.setColor(Color.YELLOW);
                         g.drawRect(x1, y1, width, height);
                         
@@ -38,7 +74,6 @@ public class MyPanelClass extends JPanel {
                         
                       //Draw a circle 
                         g.setColor(Color.LIGHT_GRAY);
-                        g.fillOval((x2-55)/2, (y2-55)/2, 55, 55); 
-                      
+                        g.fillOval((width-55)/2, (height-55)/2, 55, 55);*/
             }
 }
